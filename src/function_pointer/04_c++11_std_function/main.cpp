8e44d34308182e684557257d06550c00eb4b83cc
Fran Tena <tarod.net@gmail.com>
2016-05-28 17:10:20 +0200

    update example

--- src/function_pointer/04_c++11_std_function/main.cpp
+++ src/function_pointer/04_c++11_std_function/main.cpp
@@ -39,8 +39,8 @@ int main()
 
     // store a call to a member function
     std::function<void(const Foo&, int)> f_add_display = &Foo::print_add;
-    const Foo foo(314159);
-    f_add_display(foo, 1);
+    const Foo foo(3);
+    f_add_display(foo, 4);
 
     // store a call to a data member accessor
     std::function<int(Foo const&)> f_num = &Foo::num_;
