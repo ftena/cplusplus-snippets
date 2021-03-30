668155289b806c6e565788a1454896b69184a12b
Fran Tena <tarod.net@gmail.com>
2021-03-30 23:50:56 +0200

    chrono

--- src/chrono/chrono.cpp
+++ src/chrono/chrono.cpp
@@ -14,6 +14,7 @@ void f()
  
 int main()
 {
+    // using std::clock
     std::clock_t c_start = std::clock();
     auto t_start = std::chrono::high_resolution_clock::now();
     std::thread t1(f); 
@@ -29,4 +30,25 @@ int main()
     std::cout << "Wall clock time passed: "
               << std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count()
               << " ms\n";
+
+    // using std::literals::chrono_literals
+    using namespace std::literals::chrono_literals;
+    std::chrono::seconds halfmin = 30s;
+    std::cout << "Half a minute is " << halfmin.count() << " seconds\n"
+        "A minute and a second is " << (1min + 1s).count() << " seconds\n";
+
+    auto day = 24h;
+    auto min = 60min;
+
+    std::cout << "A day is " << day.count() << " hours\n"
+        "A day and a day is " << (day + 24h).count() << " hours\n"
+        "A day and a min is " << (day + min).count() << " min\n";
+
+    std::chrono::nanoseconds cpuCycleAt500MHz = 2ns;
+
+    std::cout << "A cpuCycleAt500MHz is " << cpuCycleAt500MHz.count() << " ns\n"
+        "Plus 5ns is " << (cpuCycleAt500MHz + 5ns).count() << " ns\n"
+        "Plus 5us is " << (cpuCycleAt500MHz + 5ns + 5us).count() << " ns\n"
+        "Plus 5ms is " << (cpuCycleAt500MHz + 5ns + 5us + 5ms).count() << " ns\n"
+        "Plus 5s is " << (cpuCycleAt500MHz + 5ns + 5us + 5ms + 5s).count() << " ns\n";
 }
